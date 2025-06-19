a8d6ea31c43b5e18a529350bd7744a7248baa768
senemogluc <cagin.senemoglu@gmail.com>
2025-06-19 13:19:23 +0300

    added config file

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    filename='discord.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s:%(name)s: %(message)s',
+    encoding='utf-8'
+)
+
+# Load environment variables from .env file
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+if not TOKEN:
+    raise ValueError("DISCORD_TOKEN not found in environment variables.")
