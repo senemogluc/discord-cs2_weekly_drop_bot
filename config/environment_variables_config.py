3b3b002a749ee45405019490ec6f59b8ce4978be
senemogluc <cagin.senemoglu@gmail.com>
2025-06-19 13:30:43 +0300

    refactor

--- config/environment_variables_config.py
+++ config/environment_variables_config.py
@@ -1,15 +1,5 @@
 import os
-import logging
 from dotenv import load_dotenv
-
-
-logging.basicConfig(
-    filename='discord.log',
-    filemode='w',
-    level=logging.DEBUG,
-    format='%(asctime)s %(levelname)s:%(name)s: %(message)s',
-    encoding='utf-8'
-)
 
 # Load environment variables from .env file
 load_dotenv()
---
+++ config/logger_config.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    filename='discord.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s:%(name)s: %(message)s',
+    encoding='utf-8'
+)
